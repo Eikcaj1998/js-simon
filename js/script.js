6bc51206406edcf85b6152e0305b286ac5eaaaf3
Eikcaj.1998 <jw220998@gmail.com>
2022-07-04 15:49:38 +0200

    css

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
 console.log('JS OK')
-/* Visualizzare in pagina 5 numeri casuali  diversi tra loro. Da lì parte un timer di 30 secondi.
-Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite i prompt().
-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
-let display = document.getElementById('countdown');
+/* Visualizzare in pagina 5 numeri casuali  diversi tra loro. 
+Da lì parte un timer di 30 secondi.
+Dopo 30 secondi l'utente deve inserire, uno alla volta, 
+i numeri che ha visto precedentemente, tramite i prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e
+ quali dei numeri da indovinare sono stati individuati. */
+const display = document.getElementById('display');
 
+let seconds = 3;
+display.innerText= seconds;
+const countDown = setInterval(function(){
+  console.log(seconds);
+display.innerText = --seconds;
+  if (seconds === 0) {
+    clearInterval(countDown)
 
-let countDown = 10;
-display = setInterval(function(){
-  console.log(countDown);
-countDown--;
-  if (countDown === 0) {
-    clearInterval(display)
-    alert('tempo scaduto')
+   prompt('qual\'è il primo numero')
+   prompt('qual\'è il secondo numero')
+   prompt('qual\'è il terzo numero')
+   prompt('qual\'è il quarto numero')
+   prompt('qual\'è il quinto numero')
   }
 }, 1000);
