8b32d5b1aec243e728333a7efb5bf727ded69782
Eikcaj.1998 <jw220998@gmail.com>
2022-07-04 18:55:50 +0200

    error

--- js/script.js
+++ js/script.js
@@ -5,20 +5,63 @@ Dopo 30 secondi l'utente deve inserire, uno alla volta,
 i numeri che ha visto precedentemente, tramite i prompt().
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e
  quali dei numeri da indovinare sono stati individuati. */
-const display = document.getElementById('display');
 
+const display = document.getElementById('display');
 let seconds = 3;
-display.innerText= seconds;
+const totalNumber = 5;
+const questNumber = document.getElementById('quest-number')
+const maxNumber = 100 ;
+display.innerText = seconds;
+let userInputNumbers = [];
+let userNumber;
+//genero numeri casuali
+function generateNumber(totalNumber, maxNumber){
+    const numbers=[]
+    while (numbers.length < totalNumber){
+        let randomNumber;
+        do{
+            randomNumber= Math.floor(Math.random() * maxNumber)+1;
+        }while(numbers.includes(randomNumber))
+        numbers.push(randomNumber)
+    }
+    return numbers
+}
+const numbers = generateNumber(totalNumber , maxNumber);
+console.log(numbers)
+    for(let i = 1 ; i <= maxNumber ; i++){
+        questNumber.innerText = numbers;
+    }
+//metto un countdown
 const countDown = setInterval(function(){
   console.log(seconds);
 display.innerText = --seconds;
   if (seconds === 0) {
-    clearInterval(countDown)
-
-   prompt('qual\'è il primo numero')
-   prompt('qual\'è il secondo numero')
-   prompt('qual\'è il terzo numero')
-   prompt('qual\'è il quarto numero')
-   prompt('qual\'è il quinto numero')
-  }
+      questNumber.classList.add("d-none")
+      clearInterval(countDown)
+    }
 }, 1000);
+//chiedo all'utendte i numeri
+setTimeout(function(){
+    for (let i = 0; i < totalNumber; i++) {
+            userNumber = prompt('scrivi i numeri che ricordi')
+            console.log(userNumber)
+            //metto una validazione
+            /* while (userInputNumbers.includes(userNumber)) {
+                alert("Hai già inserito questo numero!");
+                userNumber = parseInt(prompt("scrivi i numeri che ricordi"));
+            }
+            while (isNaN(userNumber)) {
+                alert("Devi inserire un numero...");
+                userNumber = parseInt(prompt("scrivi i numeri che ricordi"));
+            }  */
+            //Array numeri inseriti dall'utente
+             userInputNumbers.push(userNumber);    
+             if(numbers === userNumber){
+                 alert('hai indovinato corretamente tutti i numeri')
+                 questNumber.classList.remove("d-none")
+             }else{
+                 alert('hai sbagliato i numeri ritenta')
+                 questNumber.classList.remove("d-none")
+             }
+    }
+},3001)
